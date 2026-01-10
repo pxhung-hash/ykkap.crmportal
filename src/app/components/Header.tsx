ea433d41f1ccc6ca1a85ab8353bf448220833106
pxhung-hash <px.hung@ykkap.vn>
2026-01-10 21:23:40 +0700

    Update Header.tsx

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -9,7 +9,7 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "./ui/dropdown-menu";
-import logoImage from "figma:asset/1996c60364a36937d12f35eaa51d4f848d59fb2b.png";
+import logoImage from "src/assets/YKK AP Logo.png";
 
 interface HeaderProps {
   onLogout: () => void;
